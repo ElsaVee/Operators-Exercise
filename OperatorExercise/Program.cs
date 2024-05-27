da84fdf18fb5b83f5eb5d0c0ab66724f77d0b1f4
Elsa Villarreal <elsa22@att.net>
2024-05-27 15:42:28 -0500

    initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Exercise 1 declare variables
+            int a = 17;
+            int b = 4;
+            
+            //operations
+            var sum = a + b;
+            Console.WriteLine($"{ a} + { b} = { sum}");
+          
+            var subtract = a - b;
+            Console.WriteLine($"{a} - {b} = {subtract}");
+
+            var mult = a * b;
+            Console.WriteLine($"{a} * {b} = {mult}");
+
+            var div = a / b;
+            Console.WriteLine($"{a} / {b} = {sum}");
+
+            var mod = a % b;
+            Console.WriteLine($"{a} % {b} = {sum}");
+
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine($"{a}/{b} is { quotient} remainder { remainder}");
+
+
+
+
+
+
+
+
+
         }
     }
 }
